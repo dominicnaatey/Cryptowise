c2832f2165b4307743402061ac1c82c58335f405
Dom7k <dominicnaatey@gmail.com>
2021-10-02 08:37:16 +0000

    Cryptocurrencies and Search Filter

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import {Button, Menu, Typography, Avatar } from 'antd';
 import {Link} from 'react-router-dom';
-import {HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined} from '@ant-design/icons';
+import {HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons';
 
 import icon from '../images/cryptocurrency.png';
 
 const Navbar = () => {
     return (
         <div className='nav-container'>
-            <div className='logo-caontainer'>
+            <div className='logo-container'>
                 <Avatar src={icon} size='large'/>
                 <Typography.Title level={2} className='logo'>
                     <Link to='/'>Cryptowise</Link>
