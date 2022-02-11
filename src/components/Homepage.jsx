677caf581947e067522e3fb427affdd6d120d4b0
Dom7k <dominicnaatey@gmail.com>
2022-02-11 18:39:13 +0000

    Homepage Globalstats

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -11,9 +11,9 @@ const {Title} = Typography;
 
 const Homepage = () => {
     const { data, isFetching } = useGetCryptosQuery(10);
-    const  globalStats = data?.data?.stats;
+    const globalStats = data?.data?.stats;
 
-    if(isFetching) return <Loader />;
+    if (isFetching) return <Loader />;
 
 
     return (
